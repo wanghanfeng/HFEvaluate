be8d2d3769665535a923077e19a5af33aef9de5b
wanghanfeng <285360418@qq.com>
2017-04-17 15:15:18 +0800

    Initial Commit

---
+++ HFEvaluate/HFEvaluate.h
@@ -0,0 +1,19 @@
+//
+//  HFEvaluate.h
+//  HFEvaluate
+//
+//  Created by whf on 17/4/17.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HFEvaluate.
+FOUNDATION_EXPORT double HFEvaluateVersionNumber;
+
+//! Project version string for HFEvaluate.
+FOUNDATION_EXPORT const unsigned char HFEvaluateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HFEvaluate/PublicHeader.h>
+
+
