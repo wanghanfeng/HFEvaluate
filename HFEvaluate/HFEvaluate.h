880633acc998526a822f142e2b0caea301bfb985
wanghanfeng <285360418@qq.com>
2017-04-17 16:18:01 +0800

    init

--- HFEvaluate/HFEvaluate.h
+++ HFEvaluate/HFEvaluate.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char HFEvaluateVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <HFEvaluate/PublicHeader.h>
 
-
+#import "BDAppStatistics.h"
